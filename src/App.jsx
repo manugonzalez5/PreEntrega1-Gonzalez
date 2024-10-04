6984c9e47a54de75dc4b130dd32c4142c451e62c
Manuel Joaquin Gonzalez <manujoaquingonzalez5@gmail.com>
2024-10-04 20:04:03 -0300

    Implementamos MaterialUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import { Navbar } from "./components/layout/navbar/Navbar";
-import { Footer } from "./components/layout/footer/Footer";
-import { ItemListContainer } from "./components/pages/itemListContainer/ItemListContainer";
-import { Counter } from "./components/common/counter/Counter";
+import { PruebaComponentes } from "./PruebaComponentes";
+import { PruebaGrid } from "./PruebaGrid";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <ItemListContainer />
-      <Counter />
-      <Footer />
+      <PruebaGrid />
+      <PruebaComponentes />
     </div>
   );
 }
