887271316311a1ab0b5bf96c254aac8bc2be1ccc
Manuel Joaquin Gonzalez <manujoaquingonzalez5@gmail.com>
2024-10-10 16:15:54 -0300

    ingrese los productos a productsjs

--- src/components/products.js
+++ src/components/products.js
@@ -3,11 +3,38 @@
 export const products = [
     {
         id: "1",
-        title: "Acer",
+        title: "Notebook Gamer Acer Nitro V15",
         price: 999,
         stock: 10,
         imageUrl: "",
-        description: "Laptop gaming"
-        category: "Laptop",
+        description: "Laptop gaming",
+        category: "Laptop"
+    },
+    {
+        id: "2",
+        title: "HP",
+        price: 1299,
+        stock: 13,
+        imageUrl: "",
+        description: "Laptop gaming",
+        category: "Laptop"
+    },
+    {
+        id: "3",
+        title: "PC GAMER INTEL",
+        price: 999,
+        stock: 10,
+        imageUrl: "",
+        description: "PC GAMER",
+        category: "PC Escritorio"
+    },
+    {
+        id: "4",
+        title: "PC GAMER AMD",
+        price: 1200,
+        stock: 10,
+        imageUrl: "",
+        description: "PC GAMER",
+        category: "PC Escritorio"
     },
 ];
