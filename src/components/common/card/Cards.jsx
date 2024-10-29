046279d5c15ad350f12bbc33336939a0760943dc
Manuel Joaquin Gonzalez <manujoaquingonzalez5@gmail.com>
2024-10-29 12:52:54 -0300

    Cambie el button de la card

--- src/components/common/card/Cards.jsx
+++ src/components/common/card/Cards.jsx
@@ -1,15 +1,25 @@
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
 import { Link } from "react-router-dom";
 
 export const Cards = ({ title, price, stock, image, id }) => {
   return (
-    <div style={{ border: "2px solid white", width: "200px", height: "300px" }}>
+    <div
+      style={{
+        border: "2px solid white",
+        width: "200px",
+        height: "300px",
+      }}
+    >
       <img src={image} alt="" style={{ width: "100px" }} />
       <h2>{title}</h2>
       <h3>Precio: ${price}</h3>
       <h3>Stock: {stock}</h3>
-      <Link to={`/itemDetail/${id}`}>
-        <button>Ver detalle</button>
-      </Link>
+      <Stack direction="row" spacing={2}>
+        <Link to={`/itemDetail/${id}`}>
+          <Button variant="contained">Ver detalle</Button>
+        </Link>
+      </Stack>
     </div>
   );
 };
